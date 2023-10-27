83796bc6e21335b6e7f8bfabf515615b6f08282e
Andrei <andreirutty.ar20@outlook.com>
2023-10-27 14:45:08 +0400

    Added Sidebar For Mobile phones

---
+++ scripts/sidebar.js
@@ -0,0 +1,11 @@
+const hamburgerButton = document.getElementById("sidebar-button");
+const sidebar = document.querySelector(".sidebar");
+const closeButton = document.getElementById("sidebar-close-button");
+
+// Function to show and hide the side bar
+toggleSideBar = () => {
+    sidebar.classList.toggle("show");
+};
+
+hamburgerButton.addEventListener("click", toggleSideBar);
+closeButton.addEventListener("click", toggleSideBar);
