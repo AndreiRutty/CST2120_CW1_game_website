3cb6a9f242fb1a509016e12a9f299d2243f6888f
Andrei <andreirutty.ar20@outlook.com>
2023-11-17 10:29:27 +0400

    Added Level One Background + Player

--- game/Game Assets/Tilesets/A4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="A4" tilewidth="16" tileheight="16" tilecount="2160" columns="48">
- <image source="../A4.png" width="768" height="720"/>
-</tileset>
--- game/scenes/LevelOne.js
+++ game/scenes/LevelOne.js
@@ -5,9 +5,59 @@ class LevelOne extends Phaser.Scene {
 
   preload() {
     console.log("Level One Scene Loaded");
+
+    // Tiles
+    this.load.image("tiles-one", "../game/game-assets/tiles/A4.png");
+    this.load.image("tiles-two", "../game/game-assets/tiles/A5.png");
+    this.load.image("tiles-three", "../game/game-assets/tiles/Inside_C.png");
+    this.load.image("tiles-four", "../game/game-assets/tiles/Inside_D.png");
+    this.load.image("tiles-five", "../game/game-assets/tiles/Inside_E.png");
+
+    // Tilemap
+    this.load.tilemapTiledJSON(
+      "level-one-tilemap",
+      "../game/game-assets/tilemap/level1.json"
+    );
+
+    // Player
+    this.load.spritesheet("player", "../game/game-assets/player.png", {
+      frameWidth: 32,
+      frameHeight: 66,
+    });
   }
 
   create(data) {
-    this.add.text(640, 360, "Level One");
+    this.cameras.main.setZoom(2);
+
+    // Map
+    const map = this.make.tilemap({
+      key: "level-one-tilemap",
+      tileWidth: 16,
+      tileHeight: 16,
+    });
+
+    // Tile Sets
+    const tileSetOne = map.addTilesetImage("A4", "tiles-one");
+    const tileSetTwo = map.addTilesetImage("A5", "tiles-two");
+    const tileSetThree = map.addTilesetImage("Inside_C", "tiles-three");
+    const tileSetFour = map.addTilesetImage("Inside_D", "tiles-four");
+    const tileSetFive = map.addTilesetImage("Inside_E", "tiles-five");
+
+    // Layer - from lower to upper
+    const groundLayer = map.createLayer("Ground", tileSetTwo, 170, 0);
+    const wallLayer = map.createLayer("Wall", tileSetOne, 170, 0);
+    const boundaryLayer = map.createLayer("Boundary", tileSetOne, 170, 0);
+
+    // Player
+
+    this.anims.create({
+      key: "walk-down",
+      frames: this.anims.generateFrameNumbers("player", { start: 0, end: 2 }),
+      frameRate: 4,
+      repeat: -1,
+    });
+
+    this.player = this.add.sprite(640, 360, "player");
+    this.player.anims.play("walk-down", true);
   }
 }
